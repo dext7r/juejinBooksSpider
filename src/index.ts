6d5425a9c1f47fb90293cba4a69c92a5074c5b9f
dextr7 <action@h7ml.cn>
2023-08-10 22:31:10 +0800

    feat: 目录结构变更

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,19 @@
 import process from 'node:process'
 import { spiderBooks } from './spider'
-import { spiderAllBooks } from './spider/spiderAll'
-import { juejinBookSectionUrl } from './spider/task'
-import { inquirerCourse, isValidUrl, logger } from '@/utils'
+import { getAllBooksList, juejinBookSectionUrl } from './spider/task'
 import { evConfig } from '@/config'
+import { inquirerCourse, isValidUrl, logger } from '@/utils'
 
 async function main() {
   const args = process.argv.slice(2) // 排除前两个默认参数（node 和脚本文件路径）
   const url = args[0] // 第一个参数作为小册链接
   const setCookie = evConfig.cookie.length > 0
-  if (evConfig.spiderAll && evConfig.course) {
-    const book = await spiderAllBooks(evConfig.cookie)
+  if (evConfig.spiderAll === true && evConfig.course && setCookie) {
+    logger.info(`开始爬取所有已购买小册`)
+    const book = await getAllBooksList(evConfig.cookie)
     // 多线程爬取小册
-    book?.forEach((item, i) => {
-      setTimeout(() => {
-        spiderBooks(juejinBookSectionUrl + item.booklet_id, setCookie)
-      }, i * 3000) // 每个任务延迟 3 秒执行
+    book?.forEach((item) => {
+      spiderBooks(juejinBookSectionUrl + item.booklet_id, setCookie)
     })
   } else if (url && isValidUrl(url)) {
     logger.info('开始爬取指定小册')
